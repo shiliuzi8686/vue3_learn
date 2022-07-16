78fe5b052e42d1b810f1e35efdd3509dce99310b
shiliuzi8686 <2931857501@qq.com>
2022-07-16 15:42:24 +0800

    reactive、setup注意点

---
+++ 04_src_reactive函数/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <h1>个人信息</h1>
+  <h2>姓名:{{name}}</h2>
+  <h2>年龄:{{age}}</h2>
+  <!-- 模板里不需要 .value 会自动读取响应式对象中的value -->
+  <h2>另一个人的信息：{{user.name}}-{{user.age}}--{{user.a.b.c}}</h2>
+  <button @click="infoChange">修改人的信息</button>
+  <button @click="otherInfoChange">修改另一个人的信息</button>
+</template>
+
+<script>
+import { ref,reactive } from 'vue'
+  export default {
+    name:'App',
+    setup(){
+      // 数据
+      let name = ref('张三') // ref返回的是一个响应式对象
+      let age = ref(18)
+      let user = reactive({
+        name:'湖理',
+        age:1,
+        a:{
+          b:{
+            c:888
+          }
+        }
+      })
+      // 方法
+      function infoChange(){
+        // 修改响应式对象的值
+        name.value = '李四'
+        age.value = 28
+      }
+      function otherInfoChange(){
+        // 修改响应式对象的值
+        user.name = '理工'
+        user.age = 3
+        user.a.b.c = 666
+        // console.log(user.value)
+      }
+      // 返回一个对象（）常用
+      return {
+        name,
+        age,
+        user,
+        infoChange,
+        otherInfoChange
+      }
+    }
+  }
+</script>
---
+++ src/components/demo.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>个人信息</h1>
+  <h2>姓名:{{name}}</h2>
+  <h2>年龄:{{age}}</h2>
+  <button @click="emitOk">触发ok事件</button>
+</template>
+
+<script>
+import { ref,reactive } from 'vue'
+  export default {
+    name:'App',
+    props:['name','age'],
+    emits:['ok'],
+    setup(props,content){
+      console.log(content)
+      function emitOk(){
+        content.emit('ok','子组件传参')
+      }
+      // 返回一个对象（）常用
+      return {
+        emitOk
+      }
+    }
+  }
+</script>
